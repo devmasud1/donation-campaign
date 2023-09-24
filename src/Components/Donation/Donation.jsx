be8fd380194bf3df9c05b4abaa37b6299e742e21
devmasud1 <devmahmud.masu1@gmail.com>
2023-09-25 04:48:43 +0600

    donaction page see more button create dynamically & implement button toggle option

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -3,21 +3,82 @@ import DonationCard from "./DonationCard";
 
 const Donation = () => {
   const [storeData, setStoreData] = useState([]);
+  const [isShow, setIsShow] = useState(false);
+  const [noData, setNoData] = useState("");
 
   useEffect(() => {
     const localStorageData = JSON.parse(localStorage.getItem("donation"));
-    setStoreData(localStorageData);
+
+    if (!localStorageData || localStorageData.length === 0) {
+      setNoData("Your donation is empty");
+    } else {
+      setStoreData(localStorageData);
+    }
   }, []);
 
   return (
     <div className="max-w-[1440px] mx-auto my-20">
-      <div className="grid grid-cols-1 md:grid-cols-2  gap-10">
-        {storeData?.map((data) => (
-          <DonationCard key={data.id} data={data}></DonationCard>
-        ))}
-      </div>
+      {noData ? (
+        <p className="h-[70vh] flex justify-center items-center text-2xl font-bold">
+          {noData}
+        </p>
+      ) : (
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2  gap-10">
+            {isShow
+              ? storeData?.map((data) => (
+                  <DonationCard key={data.id} data={data}></DonationCard>
+                ))
+              : storeData
+                  .slice(0, 4)
+                  ?.map((data) => (
+                    <DonationCard key={data.id} data={data}></DonationCard>
+                  ))}
+          </div>
+          <div>
+            {storeData.length >= 4 ? (
+              <div className="text-center">
+                <button
+                  onClick={() => setIsShow(!isShow)}
+                  className="bg-[#009444] py-2 px-4 my-10 text-cyan-50 text-center"
+                >
+                  {isShow ? "show less" : "show more"}
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Donation;
+
+/* <div className="grid grid-cols-1 md:grid-cols-2  gap-10">
+        {isShow
+          ? storeData?.map((data) => (
+              <DonationCard key={data.id} data={data}></DonationCard>
+            ))
+          : storeData
+              .slice(0, 4)
+              ?.map((data) => (
+                <DonationCard key={data.id} data={data}></DonationCard>
+              ))}
+      </div>
+      <div>
+      {storeData.length >= 4 ? (
+        <div className="text-center">
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="bg-[#009444] py-2 px-4 my-10 text-cyan-50 text-center"
+          >
+            {isShow ? "show less" : "show more"}
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+      </div> */
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,29 @@
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Statistics = () => {
-    return (
-        <div className="max-w-[1440px] mx-auto">
-            Statistics
-        </div>
-    );
+  // const p= (31/100) * 100
+  // console.log(p)
+
+  const data = {
+    labels: ["Your Donation", "Total Donation"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [33, 66],
+        backgroundColor: ["#00C49F", "#FF444A"],
+        borderColor: ["#fff", "#fff"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="max-w-[1440px] mx-auto flex justify-center items-center">
+      <Pie data={data} />
+    </div>
+  );
 };
 
 export default Statistics;
